9b5c798728072a1124a52cc73e4ef881c3366581
LvSira <gomezvalentin541@gmail.com>
2024-05-29 21:12:26 -0300

    texto

---
+++ main.js
@@ -0,0 +1,20 @@
+let DiaSem = prompt ("Ingrese el dia de la semana: ")
+
+    if (DiaSem =="lunes") console.log("laboral"),
+    alert ("El dia es: laboral! :(")
+    else if (DiaSem=="martes") console.log("laboral"),
+    alert ("El dia es: laboral! :(")
+    else if (DiaSem=="miercoles")console.log("laboral"),
+    alert ("El dia es: laboral! :(")
+    else if (DiaSem=="jueves") console.log("laboral"),
+    alert ("El dia es: laboral! :(")
+    else if (DiaSem=="viernes") console.log("laboral"),
+    alert ("El dia es: laboral! :(")
+    else if (DiaSem=="sabado") console.log("libre"),
+    alert ("El dia es: libre!")
+    else if  (DiaSem=="domingo") console.log("libre"),
+    alert ("El dia es: libre!")
+    else console.log("dia invalido")
+    alert ("El dia ingresado no es valido");
+
+
